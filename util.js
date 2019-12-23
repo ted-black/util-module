2b75accd4ab8ee98f14968b3ffca299f95bd87c3
ted.black <ted.black@usand.com>
2019-12-23 10:25:38 -0500

    added printNumbers method...

--- util.js
+++ util.js
@@ -2,9 +2,6 @@ exports.printMsg = function() {
     console.log("This is a message from the demo package -- now here is a change.");
 }
 
-for(let i = 0; i < 10; i++){
-    console.log(i + ", is a great number....;^)");
-}
 
 exports.printFriendlyMsg = function(){
     console.log("I'm here in sub module, and I'm happy");
